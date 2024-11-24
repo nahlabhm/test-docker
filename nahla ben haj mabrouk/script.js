d38ce65c3c5fb803ccf0a85df60bc9ef5457dbbe
nahlabhm <nahlabelhadj126@gmail.com>
2024-11-24 23:07:43 +1300

    first commit

---
+++ nahla ben haj mabrouk/script.js
@@ -0,0 +1,37 @@
+ // portfolio
+ $('.gallery ul li a').click(function() {
+    var itemID = $(this).attr('href');
+    $('.gallery ul').addClass('item_open');
+    $(itemID).addClass('item_open');
+    return false;
+});
+$('.close').click(function() {
+    $('.port, .gallery ul').removeClass('item_open');
+    return false;
+});
+
+$(".gallery ul li a").click(function() {
+    $('html, body').animate({
+        scrollTop: parseInt($("#top").offset().top)
+    }, 400);
+});
+
+
+$(document).ready(function () {
+    $('.slider').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          }
+        }
+      ]
+    });
+  });
